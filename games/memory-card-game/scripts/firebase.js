63de8600d77b62ab305bbc0e689099d8336c9129
serhat_derya <88051030+SerhatDerya@users.noreply.github.com>
2025-03-23 12:28:47 +0300

    api_keys hidden and gitignore added

--- games/memory-card-game/scripts/firebase.js
+++ games/memory-card-game/scripts/firebase.js
@@ -1,15 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getDatabase, ref, set, get, child, update, remove } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBTGhmoT8vjwKYMEP23-1ob9RsGg7zp3gc",
-  authDomain: "memory-cards-5d05b.firebaseapp.com",
-  databaseURL: "https://memory-cards-5d05b-default-rtdb.firebaseio.com",
-  projectId: "memory-cards-5d05b",
-  storageBucket: "memory-cards-5d05b.firebasestorage.app",
-  messagingSenderId: "445447572363",
-  appId: "1:445447572363:web:fe94b66a3789193d37bd8e"
-};
+import { firebaseConfig } from "../config.js";
 
 // Firebase uygulamasını başlat
 const app = initializeApp(firebaseConfig);
